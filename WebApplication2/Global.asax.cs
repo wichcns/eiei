36b257230bcc887c57644874f6d1031c35f9be69
wichcns <wich_sirawich@hotmail.com>
2019-02-13 11:06:42 +0700

    Bye Bye

--- WebApplication2/Global.asax.cs
+++ WebApplication2/Global.asax.cs
@@ -13,6 +13,7 @@ namespace WebApplication2
     {
         void Application_Start(object sender, EventArgs e)
         {
+            Hello world 
             // Code that runs on application startup
             RouteConfig.RegisterRoutes(RouteTable.Routes);
             BundleConfig.RegisterBundles(BundleTable.Bundles);
